5f649e04e83820f15383c55efb493cf363f1603d
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-10-13 16:26:09 +0200

    blabla

---
+++ images/mysql/data/db_amt.sql
@@ -0,0 +1,18 @@
+DROP SCHEMA IF EXISTS db_amt;
+CREATE SCHEMA db_amt;
+SET foreign_key_checks = 0;
+USE db_amt;
+
+CREATE TABLE usr(
+	nom varchar(30) PRIMARY KEY,
+    mdp varchar(30)
+);
+
+CREATE TABLE auth_key(
+	id int auto_increment PRIMARY KEY,
+    auth_key varchar(15),
+    init_date DATE,
+    end_date date,
+    usr_nom varchar(30),
+	FOREIGN KEY (usr_nom) REFERENCES usr(nom)
+);
